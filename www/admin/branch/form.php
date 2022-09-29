1fa06cd67f7b66bbd39ba9a596be6159bc6dbec6
DESKTOP-UME5PUA\MPR <dream191161@gmail.com>
2022-09-29 15:03:11 +0900

    업체 수정 DB update 연결

--- www/admin/branch/form.php
+++ www/admin/branch/form.php
@@ -2,6 +2,43 @@
     include_once trim($_SERVER['DOCUMENT_ROOT'])."/admin/head.php";
 ?>
 
+<!-- 등록 / 수정 구분 -->
+<?php
+if ( trim($_GET['mode'])=='update' ) {
+    $S_SQL = "SELECT * FROM mpr_branch WHERE br_code = '{$_GET['code']}'; ";
+    $res = $DB -> row($S_SQL);
+?>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script>
+       
+        window.onload = function(){
+            
+            $('#exampleInputCompany2').show();
+            $('#exampleInputCode2').show();
+            $('#zip_code2').show();
+            $('#address2').show();
+            $('#reference2').show();
+            $('#exampleInputTel2').show();
+            $('#exampleInputEmail2').show();
+            $('#update_btn_div').show();
+            
+            $('#exampleInputCompany1').hide();
+            $('#exampleInputCode1').hide();
+            $('#zip_code1').hide();
+            $('#address1').hide();
+            $('#reference1').hide();
+            $('#exampleInputTel1').hide();
+            $('#exampleInputEmail1').hide();
+            $('#sign_btn_div').hide();
+        }
+
+
+    </script>
+<?php
+}
+
+?>
+
 <!-- 업체 코드 랜덤 발급 -->
 <?php
     $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -44,19 +81,22 @@
                                                 <!-- 업체 이름 -->
                                                 <div class="form-group">
                                                     <label for="exampleInputCompany1">업체명 *</label>
-                                                    <input type="text" class="form-control" id="exampleInputCompany1" name="br_name" placeholder="업체명을 입력하세요" autocomplete='off'>
+                                                    <input type="text" class="form-control" id="exampleInputCompany1" name="br_name1" placeholder="업체명을 입력하세요" autocomplete='off' style="display:block">
+                                                    <input type="text" class="form-control" id="exampleInputCompany2" name="br_name2" value="<?php echo $res['br_name']?>" autocomplete='off' style="display:none">
                                                 </div>
                                                 <!-- 업체 코드 -->
                                                 <div class="form-group">
                                                     <label for="exampleInputCode1">업체 코드 *</label>
-                                                    <input type="text" class="form-control" id="exampleInputCode1" name="br_code" value="<?php echo $random_str;?>" readonly>
+                                                    <input type="text" class="form-control" id="exampleInputCode1" name="br_code1" value="<?php echo $random_str;?>" readonly>
+                                                    <input type="text" class="form-control" id="exampleInputCode2" name="br_code2" value="<?php echo $res['br_code']?>" readonly style="display:none">
                                                 </div>
                                                 <!-- 주소 -->
                                                 <div class="form-group">
                                                     <div class="row">
                                                         <label for="event-form-table">주소</label>
                                                         <div class="input-group col-3">
-                                                            <input type="text" class="form-control" id="zip_code" name="zip_code" placeholder="우편 번호">
+                                                            <input type="text" class="form-control" id="zip_code1" name="zip_code1" placeholder="우편 번호">
+                                                            <input type="text" class="form-control" id="zip_code2" name="zip_code2" value="<?php echo $res['br_post']?>" style="display:none">
                                                             <span class="input-group-append">
                                                                 <button type="button" class="btn btn-block btn-info" onclick="address_search()">search</button>
                                                             </span>
@@ -66,7 +106,8 @@
                                                 <div class="form-group">
                                                     <div class="row">
                                                         <div class="col-4">
-                                                            <input type="text" class="form-control" id="address" name="address" placeholder="주소">
+                                                            <input type="text" class="form-control" id="address1" name="address1" placeholder="주소">
+                                                            <input type="text" class="form-control" id="address2" name="address2" value="<?php echo $res['br_addr']?>" style="display:none">
                                                         </div>
                                                         <div class="col-5">
                                                             <input type="text" class="form-control" id="detail_address" name="detail_address" placeholder="상세 주소">
@@ -74,23 +115,30 @@
                                                     </div>
                                                 </div>
                                                 <div class="form-group">
-                                                    <input type="text" class="form-control" id="reference" placeholder="참고항목" autocomplete='off'>
+                                                    <input type="text" class="form-control" id="reference1" placeholder="참고항목" autocomplete='off'>
+                                                    <input type="text" class="form-control" id="reference2" value="<?php echo $res['br_addr_etc']?>" style="display:none" placeholder="참고항목" autocomplete='off'>
                                                 </div>
                                                 <!-- 전화번호 -->
                                                 <div class="form-group">
                                                     <label for="exampleInputTel1">전화번호</label>
-                                                    <input type="text" class="form-control" name="br_tel" id="exampleInputTel1" oninput="autoHyphen(this)" maxlength="13" placeholder="전화번호를 입력하세요." autocomplete='off'>
+                                                    <input type="text" class="form-control" name="br_tel1" id="exampleInputTel1" oninput="autoHyphen(this)" maxlength="13" placeholder="전화번호를 입력하세요." autocomplete='off'>
+                                                    <input type="text" class="form-control" name="br_tel2" id="exampleInputTel2" oninput="autoHyphen(this)" maxlength="13" value="<?php echo $res['br_tel']?>" placeholder="전화번호를 입력하세요." autocomplete='off' style="display:none">
                                                 </div>
                                                 <!-- 이메일 -->
                                                 <div class="form-group">
                                                     <label for="exampleInputEmail1">이메일 </label>
-                                                    <input type="email" class="form-control" name="user_email" id="exampleInputEmail1" placeholder="이메일을 입력하세요." autocomplete='off'>
+                                                    <input type="email" class="form-control" name="user_email1" id="exampleInputEmail1" placeholder="이메일을 입력하세요." autocomplete='off'>
+                                                    <input type="email" class="form-control" name="user_email2" id="exampleInputEmail2" value="<?php echo $res['user_email']?>" placeholder="이메일을 입력하세요." autocomplete='off' style="display:none">
                                                 </div>
                                             </div>
                                     <!-- /.card-body -->
-                                            <div class="card-footer">
+                                            <div class="card-footer" id="sign_btn_div">
                                                 <a href="/admin/branch/" class="btn btn-default float-right">취소</a>
                                                 <button type="submit" class="btn btn-info float-right" name="sign_in_btn" style="margin-right: 5px;">Sign in</button>
+                                            </div>
+                                            <div class="card-footer" id="update_btn_div" style="display:none;">
+                                                <a href="/admin/branch/" class="btn btn-default float-right">취소</a>
+                                                <button type="submit" class="btn btn-info float-right" name="update_btn" style="margin-right: 5px;">Update</button>
                                             </div>
                                         </form>
                                     </div>
@@ -169,15 +217,15 @@
                         extraAddr = ' (' + extraAddr + ')';
                     }
                     // 조합된 참고항목을 해당 필드에 넣는다.
-                    document.getElementById("reference").value = extraAddr;
+                    document.getElementById("reference1").value = extraAddr;
                 
                 } else {
-                    document.getElementById("reference").value = '';
+                    document.getElementById("reference1").value = '';
                 }
 
                 // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                document.getElementById('zip_code').value = data.zonecode;
-                document.getElementById("address").value = addr;
+                document.getElementById('zip_code1').value = data.zonecode;
+                document.getElementById("address1").value = addr;
                 // 커서를 상세주소 필드로 이동한다.
                 document.getElementById("detail_address").focus();
             }
@@ -198,53 +246,128 @@
     
     $Phone = '/^(010|011|016|017|018|019)-[0-9]{3,4}-[0-9]{4}$/';
     $mail = '/^[a-zA-Z0-9]{1}[a-zA-Z0-9\-_]+@[a-z0-9]{1}[a-z0-9\-]+[a-z0-9]{1}\.(([a-z]{1}[a-z.]+[a-z]{1}[a-z]+)|([a-z]+))$/';
+    $br_name = '/^[a-zA-Z가-힣 ]+$/';
     $alert_msg = "";
     
     if(array_key_exists('sign_in_btn',$_POST)){
 
-        if(trim($_POST['zip_code'])){
-            $addr = $_POST['address']." ".$_POST['detail_address'];
-            $SQL = "INSERT INTO mpr_branch (user_id, br_code, br_name, br_post, br_addr, br_addr_etc, br_tel, user_email, reg_date, chg_date, del_yn) VALUES ('snees', '{$_POST['br_code']}', '{$_POST['br_name']}', {$_POST['zip_code']}, '{$addr}', '{$_POST['reference']}', '{$_POST['br_tel']}' , '{$_POST['user_email']}', now(), now(), 'N');";
+        if(trim($_POST['zip_code1'])){
+            $addr = $_POST['address1']." ".$_POST['detail_address'];
+            $SQL = "INSERT INTO mpr_branch (user_id, br_code, br_name, br_post, br_addr, br_addr_etc, br_tel, user_email, reg_date, chg_date, del_yn) VALUES ('snees', '{$_POST['br_code1']}', '{$_POST['br_name1']}', {$_POST['zip_code1']}, '{$addr}', '{$_POST['reference1']}', '{$_POST['br_tel1']}' , '{$_POST['user_email1']}', now(), now(), 'N');";
         }else{
-            $SQL = "INSERT INTO mpr_branch (user_id, br_code, br_name, br_tel, user_email, reg_date, chg_date, del_yn) VALUES ('snees', '{$_POST['br_code']}', '{$_POST['br_name']}', '{$_POST['br_tel']}' ,'{$_POST['user_email']}', now(), now(), 'N');";
+            $SQL = "INSERT INTO mpr_branch (user_id, br_code, br_name, br_tel, user_email, reg_date, chg_date, del_yn) VALUES ('snees', '{$_POST['br_code1']}', '{$_POST['br_name1']}', '{$_POST['br_tel1']}' ,'{$_POST['user_email1']}', now(), now(), 'N');";
         }
 
-        // 업체명 조건
-        if( trim($_POST['br_name']) && strlen(trim($_POST['br_name'])) >= 3){
+        // 업체명 입력 여부 확인
+        if( trim($_POST['br_name1']) ){
 
-            // 전화번호 입력 여부 확인
-            if( trim($_POST['br_tel']) ){
+            // 업체명 형식 확인
+            if(preg_match($br_name, $_POST['br_name1']) && strlen($_POST['br_name1']) >= 3){
 
-                // 전화번호 형식 확인
-                if( preg_match($Phone, $_POST['br_tel']) ){
-                    // 이메일 입력 여부 확인
-                    if( trim($_POST['user_email']) ){
+                // 전화번호 입력 여부 확인
+                if( trim($_POST['br_tel1']) ){
 
-                        // 이메일 형식 확인
-                        if( preg_match($mail, $_POST['user_email']) ){
+                    // 전화번호 형식 확인
+                    if( preg_match($Phone, $_POST['br_tel1']) ){
+                        // 이메일 입력 여부 확인
+                        if( trim($_POST['user_email1']) ){
+
+                            // 이메일 형식 확인
+                            if( preg_match($mail, $_POST['user_email1']) ){
+                                $statement = $DB->query($SQL);
+                                echo '<script> alert("등록되었습니다.");</script>';
+                                echo "<script>location.href='/admin/branch/index.php'</script>";
+                            }else{
+                                $alert_msg = "email_form_err";
+                            }
+
+                        }else{
                             $statement = $DB->query($SQL);
                             echo '<script> alert("등록되었습니다.");</script>';
                             echo "<script>location.href='/admin/branch/index.php'</script>";
-                        }else{
-                            $alert_msg = "email_form_err";
                         }
 
                     }else{
                         $statement = $DB->query($SQL);
-                        echo '<script> alert("등록되었습니다.");</script>';
-                        echo "<script>location.href='/admin/branch/index.php'</script>";
+                        $alert_msg = "tel_form_err";
                     }
 
                 }else{
                     $statement = $DB->query($SQL);
-                    $alert_msg = "tel_form_err";
+                    echo '<script> alert("등록되었습니다.");</script>';
+                    echo "<script>location.href='/admin/branch/index.php'</script>";
                 }
-
             }else{
-                $statement = $DB->query($SQL);
-                echo '<script> alert("등록되었습니다.");</script>';
-                echo "<script>location.href='/admin/branch/index.php'</script>";
+                $alert_msg = "c_name_form_err";
             }
+            
+
+        }else{
+            $alert_msg = "c_name_input_err";
+        }
+    }
+?>
+
+<!-- 수정 버튼 눌렀을때 -->
+
+<?php 
+    if(array_key_exists('update_btn', $_POST)){
+        
+        if(trim($_POST['zip_code2'])){
+            $addr = $_POST['address2']." ".$_POST['detail_address'];
+
+            $Up_SQL = "UPDATE mpr_branch SET user_id = 'snees', br_code = '{$_POST['br_code2']}', br_name = '{$_POST['br_name2']}', br_post = {$_POST['zip_code2']}, br_addr = '{$addr}' , br_addr_etc = '{$_POST['reference2']}' , br_tel = '{$_POST['br_tel2']}', user_email = '{$_POST['user_email2']}', chg_date = now();";
+?> 
+<script>
+    console.log('<?php echo $Up_SQL?>');
+</script>
+<?php
+        }else{
+            $Up_SQL = "UPDATE mpr_branch SET user_id = 'snees', br_code = '{$_POST['br_code2']}', br_name = '{$_POST['br_name2']}' , br_tel = '{$_POST['br_tel2']}', user_email = '{$_POST['user_email2']}', chg_date = now();";
+        }
+
+        // 업체명 입력 여부 확인
+        if( trim($_POST['br_name2']) ){
+
+            // 업체명 형식 확인
+            if(preg_match($br_name, $_POST['br_name2']) && strlen($_POST['br_name2']) >= 3){
+
+                // 전화번호 입력 여부 확인
+                if( trim($_POST['br_tel2']) ){
+
+                    // 전화번호 형식 확인
+                    if( preg_match($Phone, $_POST['br_tel2']) ){
+                        // 이메일 입력 여부 확인
+                        if( trim($_POST['user_email2']) ){
+
+                            // 이메일 형식 확인
+                            if( preg_match($mail, $_POST['user_email2']) ){
+                                $statement = $DB->query($Up_SQL);
+                                echo '<script> alert("수정되었습니다.");</script>';
+                                echo "<script>location.href='/admin/branch/index.php'</script>";
+                            }else{
+                                $alert_msg = "email_form_err";
+                            }
+
+                        }else{
+                            $statement = $DB->query($Up_SQL);
+                            echo '<script> alert("수정되었습니다.");</script>';
+                            echo "<script>location.href='/admin/branch/index.php'</script>";
+                        }
+
+                    }else{
+                        $alert_msg = "tel_form_err";
+                    }
+
+                }else{
+                    $statement = $DB->query($Up_SQL);
+                    echo '<script> alert("수정되었습니다.");</script>';
+                    echo "<script>location.href='/admin/branch/index.php'</script>";
+                }
+            }else{
+                $alert_msg = "c_name_form_err";
+            }
+            
 
         }else{
             $alert_msg = "c_name_input_err";
@@ -268,6 +391,10 @@
                 break;
             case "c_name_input_err" :
                 $("#alert_msg").text("업체명을 입력하세요.");
+                document.getElementById('modal_btn').click();
+                break;
+            case "c_name_form_err" :
+                $("#alert_msg").text("업체명은 3자 이상의 한글 또는 영문으로만 입력가능합니다.");
                 document.getElementById('modal_btn').click();
                 break;
         }
