1fa45098b2246d7baf751f357da9f668d69fa1f5
DESKTOP-UME5PUA\MPR <dream191161@gmail.com>
2022-10-07 11:18:49 +0900

    상태별로 검색_02 & 날짜 검색

--- www/admin/event/index.php
+++ www/admin/event/index.php
@@ -3,12 +3,79 @@
 
     $strSearch = trim($_GET['search']);
     $keyword = trim($_GET['input_search']);
+
+    $max_date = $DB -> row("SELECT MAX(ev_end) AS max_date FROM mpr_event");
+    $min_date = $DB -> row("SELECT MIN(ev_start) AS min_date FROM mpr_event");
+    if(trim($_GET['reservation'])){
+        $datepicker = $_GET['reservation'];
+        $startDate = explode(" ", $datepicker)[0];
+        $endDate = explode(" ", $datepicker)[2];
+        
+    }else{
+        $startDate = $min_date['min_date'];
+        $endDate = $max_date['max_date'];
+    }
     if(!trim($_GET['stat'])){
         $stat = "total";
     }else{
         $stat = $_GET['stat'];
     }
 ?>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+<script>
+    $( function() {
+        $( "#reservation" ).daterangepicker({
+            <?php 
+                
+            ?>
+            locale:{ 
+                format:'YYYY-MM-DD', //일시노출포맷
+                applyLabel :"선택",
+                cancelLabel:"취소",
+                fromLabel: "From",
+                toLabel: "To",
+                daysOfWeek:["일","월","화","수","목","금","토"],
+                monthNames:["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"]
+                },
+                startDate: "<?php echo $startDate?>",
+                endDate: "<?php echo $endDate?>",
+                drops: "auto"
+        }, function (start, end, label){
+            console.log(start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD'));
+        });
+    });
+
+
+    var stat = '<?php echo $stat?>';
+    console.log(stat);
+    switch(stat){
+        case "total" :
+            $(function(){
+                $('#st_total').css("color", "#4374D9");
+            });
+            break;
+        case "W" :
+            $(function(){
+                $('#st_w').css("color", "#4374D9");
+            });
+            break;
+        case "Y" :
+            $(function(){
+                $('#st_y').css("color", "#4374D9");
+            });
+            break;
+        case "N" :
+            $(function(){
+                $('#st_n').css("color", "#4374D9");
+            });
+            break;
+
+    }
+</script>
 
 <div class="content-wrapper">
 
@@ -38,13 +105,25 @@
                                                 <th colspan="6" style="padding:0px;">
                                                     
                                                     <div class="navbar navbar-expand navbar-white navbar-light d-flex justify-content-between" id="navbar-search2" >
-                                                        <ul style="list-style:none; margin:0px; padding:0 10px;">
-                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=total">전체</a></li>
-                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=W">진행 예정</a></li>
-                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=Y">진행중</a></li>
-                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=N">종료</a></li>
+                                                        <ul class="nav navbar-nav" style="list-style:none; margin:0px; padding:0 10px;">
+                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=total" id="st_total" style="color:#BDBDBD;">전체</a></li>
+                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=W" id="st_w" style="color:#BDBDBD;">진행 예정</a></li>
+                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=Y" id="st_y" style="color:#BDBDBD;">진행중</a></li>
+                                                            <li style="float:left; margin-right:5px;"><a href="index.php?stat=N" id="st_n" style="color:#BDBDBD;">종료</a></li>
                                                         </ul>
+                                                        
                                                         <form class="form-inline" action="index.php?stat=<?php echo $stat?>">
+                                                            <div class="form-group" >
+                                                                <div class="input-group">
+                                                                    <div class="input-group-prepend">
+                                                                    <span class="input-group-text">
+                                                                        <i class="far fa-calendar-alt"></i>
+                                                                    </span>
+                                                                    </div>
+                                                                    <input type="text" class="form-control float-right" id="reservation" name="reservation"style="height:30px; width:220px; margin-right:5px;">
+                                                                </div>
+                                                                <!-- /.input group -->
+                                                            </div>
                                                             <input type="hidden" value="<?php echo $stat?>" name="in_stat">
                                                             <div class="form-group" style="width: 90px; justify-content:right;">
                                                                 <select class="form-control select2" name="search" style="width:100%; height:30px; font-size:small; margin-right: 5px;">
@@ -83,20 +162,20 @@
 
                                                 // 검색 안했을때 테이블 가져오기
                                                 if($stat =="total"){
-                                                    $strWhere = '(1)';
+                                                    $strWhere = "ev_start >= '{$startDate}' AND ev_end <= '{$endDate}'";
                                                 }else{
                                                     $strWhere = "ev_stat = '{$stat}'";
                                                 }
-                                                
                                                 // 검색했을때 테이블 가져오기
                                                 if ( trim($_GET['search']) && trim($_GET['input_search']) ) {
-                                                    $strWhere = '';
+                                                    $strWhere = "ev_start >= '{$startDate}' AND ev_end <= '{$endDate}' AND ";
                                                     if( trim($_GET['in_stat']) != "total"){
                                                         $arryWhere[] = "ev_stat = '{$_GET['in_stat']}' and {$_GET['search']} like '%{$_GET['input_search']}%' ";
                                                     }else{
                                                         $arryWhere[] = "{$_GET['search']} like '%{$_GET['input_search']}%' ";
                                                     }
                                                     $strWhere.= implode(' and ', $arryWhere);//---- 배열로 만든다. explode('@', '문자열@문자열@문자열')
+                                                    
                                                 }
                                                 echo '<script>console.log("'.$strWhere.'");</script>';
                                                 if(isset($_GET['page'])){
